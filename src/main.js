b41281588bc58304f80b03c3d9d4b4d855638174
bri <bcamesi@gmail.com>
2023-03-17 19:40:01 -0400

    multiple routes with named router views

--- src/main.js
+++ src/main.js
@@ -6,15 +6,23 @@ import TeamsList from './components/teams/TeamsList'
 import UsersList from './components/users/UsersList'
 import TeamMembers from './components/teams/TeamMembers'
 import NotFound from './components/nav/NotFound'
+import TeamsFooter from './components/teams/TeamsFooter'
+import UsersFooter from './components/users/UsersFooter'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/teams' },
-        { path: '/teams', component: TeamsList, children: [
-            { path: ':teamId', component: TeamMembers, props: true }
+        {   name: 'teams',
+            path: '/teams', 
+            components: { default: TeamsList, footer: TeamsFooter }, 
+            children: [
+            {   name: 'team-members',
+                path: ':teamId', component: TeamMembers, props: true }
         ] },
-        { path: '/users', component: UsersList },
+        { path: '/users', components: {
+            default: UsersList, footer: UsersFooter
+        } },
         { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active'
